d18126a88cbcfd7aa9a2c3646053c9612c855b87
Your Name <mayank2157@gmail.com>
2022-12-03 16:36:04 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -36,21 +36,6 @@ cloudinary.config({
 });
 
 
-// --------------------------deployment------------------------------ ---------- //
-
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
 
 
 
